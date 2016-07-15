34ae93285ddcd5cd2e785fbe89d582df5d9b7172
Isak Sondell <isak.sondell@gmail.com>
2016-07-15 19:19:30 +0200

    from 2000

---
+++ Assets/Scripts/JawBounce.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class JawBounce : MonoBehaviour
+{
+
+    [SerializeField] private SpriteRenderer m_face = null;
+    [SerializeField] private SpriteRenderer m_jaw = null;
+
+    private bool m_opening;
+    private bool m_closing;
+    private bool m_talking;
+    public  void SetOpening(bool state) { m_opening = state; }
+
+    [SerializeField]private float m_maxOffset;
+    private float m_startPos;
+
+    [SerializeField] private float m_speed;
+    [SerializeField] private float m_rate;
+
+    void Start ()
+    {
+        m_startPos = m_jaw.transform.position.y;
+        m_maxOffset = m_startPos - m_maxOffset;
+        m_talking = false;
+        m_closing = false;
+    }
+	
+	
+	void Update ()
+    {
+        if (Input.anyKeyDown)
+        {
+            SetOpening(true);
+            Debug.Log("StartOpening");
+            m_talking = true;
+        }
+        MoveJaw();        
+	}
+
+    void MoveJaw()
+    {
+        if (!m_closing)
+        {
+            if (m_opening)
+            {
+                if (m_jaw.transform.position.y > m_maxOffset)
+                {
+                    m_jaw.transform.position = new Vector2(m_jaw.transform.position.x, m_jaw.transform.position.y + (-m_speed * Time.deltaTime));
+                }
+                else
+                {
+                    m_opening = false;
+                    m_closing = true;
+                }
+            }
+        }
+        else
+        {
+            m_opening = false;
+            if (m_jaw.transform.position.y < m_startPos)
+            {
+                m_jaw.transform.position = new Vector2(m_jaw.transform.position.x, m_jaw.transform.position.y + (m_speed * Time.deltaTime));
+            }
+            else if (m_jaw.transform.position.y >= m_startPos)
+            {
+                m_talking = false;
+                m_closing = false;
+            }
+        }
+        
+    }
+}
+
+
---
+++ Assets/Scripts/TextPrompter.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TextPrompter : MonoBehaviour
+{
+    public void SetString(string str) {PushChars(str); }
+    private List<char> m_letterList; 
+
+    [SerializeField] private Text m_text;
+    private bool m_isPrinting;
+    [SerializeField] private float m_printSpeed;
+    private float m_printTime;
+    private int m_printIndex;
+
+    private JawBounce m_jawBouncer = null;
+
+    void Start()
+    {
+        if(m_jawBouncer == null)
+        {
+            m_jawBouncer = gameObject.GetComponent<JawBounce>();
+        }
+        m_printTime = 0;
+    }
+
+    private void PushChars(string str)
+    {
+        m_letterList = new List<char>();
+        char[] chArray = str.ToCharArray();
+
+        for (int i = 0; i < chArray.Length; i++)
+        {
+            m_letterList.Add(chArray[i]);
+        }
+
+        m_printIndex = 0;
+    }
+
+    private void PrintChars()
+    {
+        m_printTime += Time.deltaTime * Random.Range(0.9f, 1.1f);
+        if(m_letterList.Count > m_printIndex)
+        {
+            if (m_printTime > m_printSpeed)
+            {
+                m_jawBouncer.SetOpening(true);
+                m_text.text += m_letterList[m_printIndex].ToString();
+                m_printIndex++;
+     
+                Debug.Log(m_printIndex);
+            }
+        }   
+    }
+
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            m_isPrinting = true;
+            SetString("okej, här kommer texten");
+        }
+
+
+        if (m_isPrinting)
+        {
+            PrintChars();
+        }
+    }
+}
